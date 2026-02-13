5f508259cdd82cfeb2e20eb5a9cff7e7ee13744a
StevenJohnWeaver <steve.weaver@hashicorp.com>
2026-02-13 11:38:28 -0500

    Update main.tf

--- modules/cluster/main.tf
+++ modules/cluster/main.tf
@@ -1,29 +1,27 @@
 terraform {
   required_providers {
-    aws    = { source = "hashicorp/aws",    version = "~> 6.0" }
+    aws = { source = "hashicorp/aws",    version = "~> 6.0" }
     random = { source = "hashicorp/random", version = "~> 3.5" }
   }
 }
 
 module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  # Keep your existing constraint (or the exact version you had when it worked).
+  # The key here is using the legacy inputs this runtime expects.
+  version = "~> 21.0"
 
-  source = "github.com/terraform-aws-modules/terraform-aws-eks?ref=v21.11.0"
+  # NOTE: Using 'name' here to match the module schema Stacks is resolving today.
+  name                = var.cluster_name
+  kubernetes_version  = var.kubernetes_version
+  subnet_ids          = var.subnet_ids
+  vpc_id              = var.vpc_id
 
-  # source  = "terraform-aws-modules/eks/aws"
-  # version = "21.11.0"
+  # Legacy endpoint args (the "cluster_endpoint_*" variants triggered 'unsupported' earlier)
+  endpoint_public_access  = true
+  endpoint_private_access = true
 
-  cluster_name                       = var.cluster_name
-  kubernetes_version                 = var.kubernetes_version
-  subnet_ids                         = var.subnet_ids
-  vpc_id                             = var.vpc_id
-
-  # Admin + endpoint access
   enable_cluster_creator_admin_permissions = true
-  cluster_endpoint_public_access  = true
-  cluster_endpoint_private_access = true
-
-  # endpoint_public_access  = true
-  # endpoint_private_access = true  # optional, but recommended
 
   eks_managed_node_groups = {
     demo = {
@@ -31,30 +29,44 @@ module "eks" {
       max_size       = 1
       min_size       = 1
       instance_types = ["t3.small"]
+
+      # Give the nodegroup more time to become healthy while the separate 'auth' component
+      # writes aws-auth (if that's not immediate).
+      create_timeout = "40m"
+      update_timeout = "60m"
+      delete_timeout = "40m"
     }
   }
-  ############################################
-  # EKS Core Addons (required)
-  ############################################
-  cluster_addons = {
-    vpc-cni = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
-    kube-proxy = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
-    coredns = {
-      most_recent       = true
-      resolve_conflicts = "OVERWRITE"
-    }
-  }
-  
-  ############################################
-  # AWS Auth (critical!!)
-  ############################################
-  manage_aws_auth = true
 
   tags = { demo = "stacks" }
+}
+
+############################################
+# EKS Addons as native AWS resources
+# (module version independent)
+############################################
+
+# Ensure addons get created after the cluster exists
+resource "aws_eks_addon" "vpc_cni" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "vpc-cni"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  depends_on                  = [module.eks]
+}
+
+resource "aws_eks_addon" "kube_proxy" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "kube-proxy"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  depends_on                  = [module.eks]
+}
+
+resource "aws_eks_addon" "coredns" {
+  cluster_name                = var.cluster_name
+  addon_name                  = "coredns"
+  resolve_conflicts_on_create = "OVERWRITE"
+  resolve_conflicts_on_update = "OVERWRITE"
+  depends_on                  = [module.eks]
 }
